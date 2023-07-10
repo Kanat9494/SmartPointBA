78c6d26836b1e7108140fdee9d07af52383b0327
kana2022 <kkanat94@gmail.com>
2023-07-10 09:24:57 +0600

    Add project files.

---
+++ SmartPointBA/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartPointBA;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SmartPointBA/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmartPointBA;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
